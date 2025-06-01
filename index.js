7bf8ff6a40232a949199e0aad29669d8252d65be
Yogendra Gothwal <rohitverma43434@gmail.com>
2025-06-02 03:49:06 +0530

    added cors

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ require('dotenv').config();
 app.use(express.json());
 const cors = require('cors');
 app.use(cors({
-    origin: '*'
-}))
+    origin: "https://your-frontend.onrender.com",
+    credentials: true
+}));
+
 const PORT = 5000
 const mongoose = require('mongoose');
 
